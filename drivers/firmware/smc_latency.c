432a8b143b20b044426342574a600a467e3d4367
Marek Bykowski <marek.bykowski@gmail.com>
2019-04-18 03:07:48 -0500

    drivers/firmware: arch64: Add more SMC calls
    The more SMC calls got added in to test all the SMC services added in from
    ATF Advisory back-porting through SMC and PSCI upgrade from v1.0 to v1.1.
    
    Signed-off-by: Marek Bykowski <marek.bykowski@gmail.com>

--- drivers/firmware/smc_latency.c
+++ drivers/firmware/smc_latency.c
@@ -22,7 +22,7 @@
 #include <linux/seq_file.h>
 #include <uapi/linux/psci.h>
 
-#define SMC_DEBUG 0
+#define SMC_DEBUG 1
 #if SMC_DEBUG
 #define smc_dbg(...) pr_info(__VA_ARGS__)
 #define seq_printf(...)
@@ -44,71 +44,40 @@ static bool all_cpus;
 module_param(all_cpus, bool, 0644);
 MODULE_PARM_DESC(all_cpus, "Run smc call on executing or all cpus (default: all");
 
-void smc_call_v10(void *arg);
-void smc_call_v11(void *arg);
+void smc_call_v10_invoke_psci_ver(void *arg);
+void smc_call_v11_execute_mitigation_to_CVE_2017_5715(void *arg);
+void smc_call_v10_smccc_ver_from_psci_feature(void *arg);
+void smc_call_v11_smccc_ver_directly_less_safe(void *arg);
+void smc_call_v11_determine_availability_of_arm_arch_service(void *arg);
+void smc_call_v11_psci_v11_query_if_memprotect_is_enabled(void *args);
+void smc_call_v11_psci_v11_query_if_mem_reset2_is_enabled(void *args);
 
 smc_cb_t smc_call[] = {
-	smc_call_v10,
-	smc_call_v11
+	smc_call_v10_invoke_psci_ver,
+	smc_call_v11_execute_mitigation_to_CVE_2017_5715,
+	smc_call_v10_smccc_ver_from_psci_feature,
+	smc_call_v11_smccc_ver_directly_less_safe,
+	smc_call_v11_determine_availability_of_arm_arch_service,
+	smc_call_v11_psci_v11_query_if_memprotect_is_enabled,
+	smc_call_v11_psci_v11_query_if_mem_reset2_is_enabled
 };
 
-#define SMC_VARIANTS ARRAY_SIZE(smc_call)
 #define SMC_VARIANT(n) n
 #define for_each_smc(smc) \
-	for ((smc) = 0; (smc) < SMC_VARIANTS; (smc)++)
+	for ((smc) = 0; (smc) < ARRAY_SIZE(smc_call); (smc)++)
 
-unsigned long __percpu *time[SMC_VARIANTS];
-
-/* SMCCC 1.0 */
-void smc_call_v10(void *arg)
-{
-	struct pt_regs _regs = {0}, *regs = &_regs;
-	int smc = *(unsigned int *)arg;
-	struct arm_smccc_res res;
-	ktime_t start, end;
-
-	regs->regs[0] = PSCI_0_2_FN_PSCI_VERSION;
-
-	start = ktime_get();
-	arm_smccc_smc(regs->regs[0], regs->regs[1], regs->regs[2],
-				  regs->regs[3], 0, 0, 0, 0, &res);
-	end = ktime_get();
-
-	this_cpu_write(*time[smc], ktime_to_ns(ktime_sub(end, start)));
-	smc_dbg("cpu%d %lu ns (ret %#lx)\n",
-				smc,
-				this_cpu_read(*time[smc]),
-				res.a0);
-}
-
-/*  SMCCC 1.1 */
-void smc_call_v11(void *arg)
-{
-	int smc = *(unsigned int *)arg;
-	ktime_t start, end;
-
-	start = ktime_get();
-	arm_smccc_1_1_smc(ARM_SMCCC_ARCH_WORKAROUND_1, NULL);
-	end = ktime_get();
-
-	this_cpu_write(*time[smc], ktime_to_ns(ktime_sub(end, start)));
-	smc_dbg("cpu%d %lu ns (ret %s)\n",
-				smc,
-				this_cpu_read(*time[smc]),
-				"RET0 in smc call");
-}
+unsigned long __percpu *time[ARRAY_SIZE(smc_call)];
 
 void
 zeroise_times(void)
 {
 	int cpu, smc;
 
-	for_each_smc(smc) {
-		for_each_online_cpu(cpu) {
+	for_each_smc(smc)
+		for_each_online_cpu(cpu)
 			*per_cpu_ptr(time[smc], cpu) = 0;
-		}
-	}
 }
+
 
 static void run_test(void)
 {
@@ -139,6 +108,190 @@ static void run_test(void)
 	}
 }
 
+void smc_call_v10_invoke_psci_ver(void *arg)
+{
+	struct pt_regs _regs = {0}, *regs = &_regs;
+	int smc = *(unsigned int *)arg;
+	struct arm_smccc_res res;
+	ktime_t start, end;
+
+	regs->regs[0] = PSCI_0_2_FN_PSCI_VERSION;
+
+	preempt_disable();	
+
+	start = ktime_get();
+	arm_smccc_smc(regs->regs[0], regs->regs[1], regs->regs[2],
+				  regs->regs[3], 0, 0, 0, 0, &res);
+	end = ktime_get();
+
+	this_cpu_write(*time[smc], ktime_to_ns(ktime_sub(end, start)));
+	smc_dbg("%s: cpu%d %lu ns (ret %#lx)\n",
+				__func__, smp_processor_id(),
+				this_cpu_read(*time[smc]),
+				res.a0);
+
+	preempt_enable();	
+}
+
+void smc_call_v11_execute_mitigation_to_CVE_2017_5715(void *arg)
+{
+	int smc = *(unsigned int *)arg;
+	ktime_t start, end;
+
+	preempt_disable();	
+
+	start = ktime_get();
+	arm_smccc_1_1_smc(ARM_SMCCC_ARCH_WORKAROUND_1, NULL);
+	end = ktime_get();
+
+	this_cpu_write(*time[smc], ktime_to_ns(ktime_sub(end, start)));
+	smc_dbg("%s: cpu%d %lu ns (%s)\n",
+				__func__, smp_processor_id(),
+				this_cpu_read(*time[smc]),
+				"no return value");
+
+	preempt_enable();	
+}
+
+/* Retrieve the implemented version of the SMCCC from PSCI services.
+ * It is the safest way to retrive this as compatible with the system not
+ * having the SMCCC v1.1 implemented. */
+void smc_call_v10_smccc_ver_from_psci_feature(void *arg)
+{
+	int smc = *(unsigned int *)arg;
+	struct arm_smccc_res res;
+	ktime_t start, end;
+
+	preempt_disable();	
+
+	start = ktime_get();
+	arm_smccc_smc(PSCI_1_0_FN_PSCI_FEATURES,
+			      ARM_SMCCC_VERSION_FUNC_ID, 0, 0, 0, 0, 0, 0, &res);
+	if (res.a0 != PSCI_RET_NOT_SUPPORTED) {
+		arm_smccc_smc(ARM_SMCCC_VERSION_FUNC_ID, 0, 0, 0, 0, 0, 0, 0, &res);
+	}
+	end = ktime_get();
+
+	this_cpu_write(*time[smc], ktime_to_ns(ktime_sub(end, start)));
+	smc_dbg("%s: cpu%d %lu ns (ret %#lx)\n",
+				__func__, smp_processor_id(),
+				this_cpu_read(*time[smc]),
+				res.a0);
+
+	preempt_enable();
+}
+
+/* Retrieve the implemented version of the SMCCC */
+void smc_call_v11_smccc_ver_directly_less_safe(void *arg)
+{
+	int smc = *(unsigned int *)arg;
+	struct arm_smccc_res res;
+	ktime_t start, end;
+
+	preempt_disable();	
+
+	start = ktime_get();
+	arm_smccc_1_1_smc(0x80000000, &res);
+	end = ktime_get();
+
+	this_cpu_write(*time[smc], ktime_to_ns(ktime_sub(end, start)));
+	smc_dbg("%s: cpu%d %lu ns (ret %#lx)\n",
+				__func__, smp_processor_id(),
+				this_cpu_read(*time[smc]),
+				res.a0);
+
+	preempt_enable();
+}
+
+/* Determine the availability and capability of Arm Architecture Service
+ * functions
+ */
+void smc_call_v11_determine_availability_of_arm_arch_service(void *arg)
+{
+	int smc = *(unsigned int *)arg;
+	struct arm_smccc_res res;
+	ktime_t start, end;
+
+	preempt_disable();	
+
+	start = ktime_get();
+	arm_smccc_1_1_smc(0x80000001/*SMCCC_ARCH_FEATURES*/,
+			0x80000001/*again in x1*/,
+			&res);
+	end = ktime_get();
+
+	this_cpu_write(*time[smc], ktime_to_ns(ktime_sub(end, start)));
+	smc_dbg("%s: cpu%d %lu ns (ret %#lx)\n",
+				__func__, smp_processor_id(),
+				this_cpu_read(*time[smc]),
+				res.a0);
+
+	preempt_enable();
+}
+
+/* MEM_PROTECT function provides protection against cold reboot attacks, by
+ * ensuring that memory is overwritten before it is handed over to an
+ * operating system loader. No callback registered for Axxia.
+ */
+void smc_call_v11_psci_v11_query_if_memprotect_is_enabled(void *arg)
+{
+	int smc = *(unsigned int *)arg;
+	struct arm_smccc_res res;
+	s32 val __maybe_unused;
+	ktime_t start, end;
+
+	preempt_disable();	
+
+	start = ktime_get();
+	arm_smccc_1_1_smc(0x84000013/*MEM_PROTECT*/, &res);
+	end = ktime_get();
+
+	val = (s32) res.a0;
+
+	this_cpu_write(*time[smc], ktime_to_ns(ktime_sub(end, start)));
+	smc_dbg("%s: cpu%d %lu ns (ret %d)\n",
+				__func__, smp_processor_id(),
+				this_cpu_read(*time[smc]),
+				val);
+
+	preempt_enable();
+}
+
+/* SYSTEM_RESET2 function extends SYSTEM_RESET. It provides:
+ * - Architectural reset definitions.
+ * - Support for vendor-specific resets.
+ */
+void smc_call_v11_psci_v11_query_if_mem_reset2_is_enabled(void *arg)
+{
+	int smc = *(unsigned int *)arg;
+	struct arm_smccc_res res[2] = {0};
+	s32 val[2] = {0};
+	ktime_t start, end;
+
+	preempt_disable();	
+
+	start = ktime_get();
+#if 1
+	arm_smccc_1_1_smc(0x84000012/*SYSTEM_RESET2 for SMC32 version*/,
+			0<<31/*arch. resets, bit31 0*/,
+			&res[0]);
+#endif
+	arm_smccc_1_1_smc(0xC4000012/*SYSTEM_RESET2 for SMC64 version*/,
+			0<<31/*arch. resets, bit31 0*/,
+			&res[1]);
+	end = ktime_get();
+
+	val[0] = (s32) res[0].a0;
+	val[1] = (s32) res[1].a0;
+
+	this_cpu_write(*time[smc], ktime_to_ns(ktime_sub(end, start)));
+	smc_dbg("%s: cpu%d %lu ns (ret32 %d ret64 %d)\n",
+				__func__, smp_processor_id(),
+				this_cpu_read(*time[smc]),
+				val[0], val[1]);
+
+	preempt_enable();
+}
 
 static int proc_smc_show(struct seq_file *m, void *v)
 {
@@ -198,7 +351,7 @@ static int __init smctest_init(void)
 }
 late_initcall(smctest_init);
 
-static void __exit dmatest_exit(void)
+static void __exit smctest_exit(void)
 {
 	unsigned int smc;
 
@@ -214,7 +367,7 @@ static void __exit dmatest_exit(void)
 	}
 }
 
-module_exit(dmatest_exit);
+module_exit(smctest_exit);
 
 MODULE_AUTHOR("Marek Bykowski");
 MODULE_LICENSE("GPL v2");
